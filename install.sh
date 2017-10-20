be9f86318aee21337dbec9d912feb3aa77b15243
eboumendil <eboumendil@users.noreply.github.com>
2017-10-19 22:13:16 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo "JETTY_VERSION=7.0.2.v20100331
+mkdir demo
+cd demo
+wget http://10.254.1.39:8080/job/HelloBuild/lastSuccessfulBuild/artifact/HelloWorld.java
+wget -U none http://repo1.maven.org/maven2/org/eclipse/jetty/aggregate/jetty-all/$JETTY_VERSION/jetty-all-$JETTY_VERSION.jar
+wget -U none http://repo1.maven.org/maven2/javax/servlet/servlet-api/2.5/servlet-api-2.5.jar
+javac -cp servlet-api-2.5.jar:jetty-all-$JETTY_VERSION.jar HelloWorld.java
+nohup java -cp .:servlet-api-2.5.jar:jetty-all-$JETTY_VERSION.jar HelloWorld &" >> /etc/rc.local
