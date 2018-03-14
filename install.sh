ec00d1eb56f34342da57675349467ee5f3def711
eboumendil <eboumendil@users.noreply.github.com>
2018-03-13 17:49:04 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ cd demo
 wget http://10.210.1.17:8080/job/build/lastSuccessfulBuild/artifact/HelloWorld.java
 wget -U none http://repo1.maven.org/maven2/org/eclipse/jetty/aggregate/jetty-all/$JETTY_VERSION/jetty-all-$JETTY_VERSION.jar
 wget -U none http://repo1.maven.org/maven2/javax/servlet/servlet-api/2.5/servlet-api-2.5.jar
+yum install -y java-devel
 javac -cp servlet-api-2.5.jar:jetty-all-$JETTY_VERSION.jar HelloWorld.java
 nohup java -cp .:servlet-api-2.5.jar:jetty-all-$JETTY_VERSION.jar HelloWorld &" >> /etc/rc.local
